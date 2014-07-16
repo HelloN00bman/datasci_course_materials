01b40257809207939b34f18670c27ab18e8bd1a4
HelloN00bman <newmanba@indiana.edu>
2014-07-15 23:41:10 -0500

    Determine tweet sentiment in tweet_sentiment.py

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,18 +1,37 @@
 import sys
 import json
 
-def hw():
-    print 'Hello, world!'
+def initDictFromFile(f, d):
+    newDict = {} 
+    for line in f:
+      k, v  = line.split(d)
+      newDict[k] = int(v)
+    return newDict
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def getMsgs(f):
+    ls = [] 
+    for line in f:
+      j  = json.loads(line).get('text',"").split(" ")
+      if j[0] != '':
+          ls.append(j)
+    return ls
+
+def getSentiment(msgs, sLs):
+    for msg in msgs:
+        s = 0
+        for w in msg:
+            if w in sLs.keys():
+                s += sLs[w]
+        print s
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    afinnfile = sent_file
+    scores = initDictFromFile(afinnfile, '\t')
+    out = tweet_file
+    msgLs = getMsgs(out)
+    getSentiment(msgLs, scores)
 
 if __name__ == '__main__':
     main()
